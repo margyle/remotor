29fa55c7095435dc85f62831f3ce760856175063
Jamie Bull <jamie.bull@kiwi.com>
2018-09-24 18:00:58 +0200

    whitenoise: fix backwards non-compatibility issue
    The `whitenoise.django.GzipManifestStaticFilesStorage` alias has now
    been removed. Instead you should use the correct import path:
    `whitenoise.storage.CompressedManifestStaticFilesStorage`
    
    http://whitenoise.evans.io/en/stable/changelog.html#v4-0

--- remotorboard/settings.py
+++ remotorboard/settings.py
@@ -145,7 +145,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
